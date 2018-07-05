99fe22650040cfe63bfc7946dd0fb47f0e47904a
Mukund Agarwal <mukundagarwal@Ushma-Yadav.local>
2018-07-05 15:58:53 +0530

    updated the output

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,21 +1,16 @@
 import sys
 
 def pascal(n):
-    l=[]
     for line in range(1,n+1):
         c=1
-        x=[]
         print(' '*(n-line+1),end='')
         for i in range(1,line+1):
-            x.append(c)
             print(c,end=' ')
             c=c*(line-i)//(i)
-        l.append(x)
-        print('\n')
-    #print(l)
+        print()
 
 if __name__=='__main__':
     if len(sys.argv)==2:
-        #n=int(input())
         pascal(int(sys.argv[1]))
-
+    else:
+        print("Enter no of rows of pascal triangle")
