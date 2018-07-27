8e46da1b0f1e1a67ae67d59ab89c1982d083c3da
mukund26 <m.agarwalhp@gmail.com>
2018-07-27 16:59:30 +0530

    class based approach

---
+++ Tree.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000
+
+int scan(){
+	register int c = getchar();
+	register int n=0,neg=0;
+	for( ; (c<48||c>57)&&c!='-' ; c=getchar());
+	if(c=='-') { neg=1; c = getchar(); }
+	for( ;c>=48&&c<=57;c=getchar()){
+		n = (n<<1) + (n<<3) + c - 48;
+	}	
+	if(neg) return -n;
+	else return n;
+}
+
+struct node{
+	int data;
+	node *left,*right;
+};
+
+class tree{
+	public:
+		node *root;
+		tree(){
+			root=NULL;
+		}
+		node* newnode(int x){
+			node *temp=new node;
+			temp->data=x;
+			temp->left=temp->right=NULL;
+			return temp;
+		}
+		node* insert(node *root,int x){
+			if(root==NULL) root=newnode(x);
+			else if(root->data>=x) root->left=insert(root->left,x);
+			else root->right = insert(root->right,x);
+			return root; 
+		}
+		void print(node *root){
+			if (root==NULL) return;
+			if(root->left!=NULL) print(root->left);
+			cout<<root->data<<" ";
+			if(root->right!=NULL) print(root->right); 
+		}		
+};
+
+
+int main(){
+	int a=scan();
+	tree t;
+	int x;
+	while(a--){
+		x=scan();
+		t.root=t.insert(t.root,x);
+	}
+	cout<<t.root<<endl;
+	t.print(t.root);
+	return 0;
+}
